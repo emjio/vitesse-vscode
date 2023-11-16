3339c5d95e8ea0559f3dd5ddcb4944c97c4066f8
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-11-16 22:43:54 +0800

    chore: update index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { message } from '@vscode-use/utils'
+import type { Disposable, ExtensionContext } from 'vscode'
 
-export function activate() {
+export function activate(context: ExtensionContext) {
+  const disposes: Disposable[] = []
+
   message.info('Hello')
+  context.subscriptions.push(...disposes)
 }
 
 export function deactivate() {
